d45ff0b0223808dc79fe666530b92be8a189023b
Esther <estherlecina@gmail.com>
2026-06-08 17:31:00 +0200

    Feat: Crea menú principal en App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
   <main class="main">
     <router-view />
   </main>
+  <nav class="nav-bar">
+    <router-link to="/" class="fas fa-home has-color-light"></router-link>
+    <router-link to="/cart" class="fas fa-shopping-cart has-color-light"></router-link>
+    <router-link to="/profile" class="fas fa-user has-color-light"></router-link>
+  </nav>
 </template>
 
 <style scoped>
@@ -12,5 +17,13 @@
   flex: 1;
   overflow-y: auto;
   padding: 1rem;
+}
+
+.nav-bar {
+  background-color: var(--dark);
+  height: 3rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
